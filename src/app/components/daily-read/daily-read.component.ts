663292609bd7dd2579997199d377011902a874aa
stepan <s.leas@developmentaid.org>
2024-01-21 15:42:12 +0200

    create necessary pages and components

---
+++ src/app/components/daily-read/daily-read.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-daily-read',
+  standalone: true,
+  imports: [],
+  templateUrl: './daily-read.component.html',
+  styleUrl: './daily-read.component.scss'
+})
+export class DailyReadComponent {
+
+}
