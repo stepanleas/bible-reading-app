b349e394f51f05df711163a84dfd24239723b5e4
stepan <s.leas@developmentaid.org>
2024-02-10 14:44:17 +0200

    add footer and header inside app component. Create first routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { TodayComponent } from "./pages/today/today.component";
+import { PlanComponent } from "./pages/plan/plan.component";
+import { SettingsComponent } from "./pages/settings/settings.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'today', component: TodayComponent },
+  { path: 'plan', component: PlanComponent },
+  { path: 'settings', component: SettingsComponent },
+];
